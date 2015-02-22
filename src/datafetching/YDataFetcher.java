d7b3f5c1e03f2b585fc26ea5b35d48791824c37e
nchinmay <nchinmay@hotmail.com>
2015-02-22 00:26:35 -0500

    Added pagination to YQL requests to query SYMBOLS_PER_QUERY no. of symbols at a time and try querying for each symbol at the most TRIES_PER_SYMBOL times

--- src/datafetching/YDataFetcher.java
+++ src/datafetching/YDataFetcher.java
@@ -8,6 +8,7 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -23,33 +24,76 @@ import datautil.CsvFileHelper;
 import datautil.RunHelper;
 
 /**
- * This thing just gets Publicly Available fundamental data from Yahoo and dumps to CSV/some other data container. It does nothing more nothing less and is not intended for commercial use.
+ * This thing just gets Publicly Available fundamental data from Yahoo and dumps
+ * to CSV/some other data container. It does nothing more nothing less and is
+ * not intended for commercial use.
  * 
  * @- TODO - There's unsafe calls all over the place. Clean them
  * @- TODO - No Validations anywhere at all. Add them
- * @- TODO - Alerts for bad data e.g. Yahoo thinks VTR has a Market Cap of 25.41 Trillion on 20150220
+ * @- TODO - Alerts for bad data e.g. Yahoo thinks VTR has a Market Cap of 25.41
+ *    Trillion on 20150220
  */
 public class YDataFetcher
 {
 	public static final String SYMBOL_FILE = "symbols.txt";
+	public static final int SYMBOLS_PER_QUERY = 50;
+	public static final int TRIES_PER_SYMBOL = 3;
 
 	public static void main(String[] args) throws Exception
 	{
 		Set<String> symbols = readSymbolsFromFile();
-		InputStream is = YDataFetcher.getYQXMLData(symbols);
-		YDataFetcher.parseYQXMLQuery(is);
+		getYahooData(symbols);
 	}
 
-	public static String getBaseUrl(Set<String> symbols)
+	public static void getYahooData(final Set<String> symbols)
+	{
+		if (symbols != null)
+		{
+			Set<String> symbolsForThisQuery = new HashSet<String>();
+			Set<String> unprocessedSymbols = new HashSet<String>();
+			Iterator<String> iter = symbols.iterator();
+			while (iter.hasNext())
+			{
+				symbolsForThisQuery.add(iter.next());
+				if (symbolsForThisQuery.size() == SYMBOLS_PER_QUERY || !iter.hasNext())
+				{
+					try
+					{
+						for (int tries = 0; tries < TRIES_PER_SYMBOL && !symbolsForThisQuery.isEmpty(); ++tries)
+						{
+							InputStream is = YDataFetcher.getYQXMLInputStream(symbolsForThisQuery);
+							Set<String> processedSymbols = YDataFetcher.parseYQXMLQueryAndReturnProcessedSymbols(is);
+							if (processedSymbols != null)
+								symbolsForThisQuery.removeAll(processedSymbols);
+						}
+						unprocessedSymbols.addAll(symbolsForThisQuery);
+						symbolsForThisQuery.clear();
+					}
+					catch (Exception e)
+					{
+						e.printStackTrace();
+					}
+				}
+			}
+			for (String s : unprocessedSymbols)
+			{
+				System.out.println("Failed to process symbol - " + s);
+			}
+		}
+	}
+
+	public static String getBaseUrl(final Set<String> symbols)
 	{
 		StringBuilder tickers = null;
 		for (String symbol : symbols)
 		{
-			tickers = tickers == null ? tickers = new StringBuilder("\"" + symbol + "\"") : tickers.append(",\"" + symbol + "\"");
+			tickers = tickers == null ? tickers = new StringBuilder("\"" + symbol + "\"") : tickers.append(",\""
+					+ symbol + "\"");
 		}
 		if (tickers != null)
 		{
-			StringBuilder baseUrl = new StringBuilder("http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(");
+			StringBuilder baseUrl = new StringBuilder(
+					"http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(");
 			baseUrl.append(tickers);
 			baseUrl.append(")&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys");
 			return baseUrl.toString();
@@ -57,17 +101,19 @@ public class YDataFetcher
 		return null;
 	}
 
-	public static InputStream getYQXMLData(final Set<String> symbols) throws Exception
+	public static InputStream getYQXMLInputStream(final Set<String> symbols) throws Exception
 	{
 		// Make Connection And Request Data in XML Format
 		URL url = new URL(getBaseUrl(symbols));
 		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-		if (conn.getResponseCode() != 200) throw new IOException(conn.getResponseMessage());
+		if (conn.getResponseCode() != 200)
+			throw new IOException(conn.getResponseMessage());
 		return conn.getInputStream();
 	}
 
-	public static void parseYQXMLQuery(InputStream inputStream) throws Exception
+	public static Set<String> parseYQXMLQueryAndReturnProcessedSymbols(InputStream inputStream) throws Exception
 	{
+		Set<String> processedSymbols = null;
 		if (inputStream != null)
 		{
 			// Create Document And Parse XML Stream
@@ -83,17 +129,26 @@ public class YDataFetcher
 			NodeList quoteNodes = dom.getElementsByTagName("quote");
 			if (quoteNodes != null && quoteNodes.getLength() > 0)
 			{
+				processedSymbols = new HashSet<String>();
 				for (int i = 0; i < quoteNodes.getLength(); ++i)
 				{
 					Element quote = (Element) quoteNodes.item(i);
+
+					// CSV data - to be removed once capnp layer is ready
 					YData data = parseYDXMLQuote(quote);
-					CsvFileHelper.writeAsCsvFile(RunHelper.getTodayDataDirectory(), data.getRowKey() + ".csv", ',', data);
+					CsvFileHelper.writeAsCsvFile(RunHelper.getTodayDataDirectory(), data.getRowKey() + ".csv", ',',
+							data);
+
 					// Cap'n Proto data
 					parseYDXMLQuoteToCapnp(quote);
+
+					// Add to processed symbols
+					processedSymbols.add(data.getSymbol());
 					System.out.println(data);
 				}
 			}
 		}
+		return processedSymbols;
 	}
 
 	public static YData parseYDXMLQuote(Element quote)
@@ -107,7 +162,8 @@ public class YDataFetcher
 		d.setFiftydayMovingAverage(convertToDouble(getValueFromXML(quote, "FiftydayMovingAverage")));
 		d.setChangeFromFiftydayMovingAverage(convertToDouble(getValueFromXML(quote, "ChangeFromFiftydayMovingAverage")));
 		d.setTwoHundreddayMovingAverage(convertToDouble(getValueFromXML(quote, "TwoHundreddayMovingAverage")));
-		d.setChangeFromTwoHundreddayMovingAverage(convertToDouble(getValueFromXML(quote, "ChangeFromTwoHundreddayMovingAverage")));
+		d.setChangeFromTwoHundreddayMovingAverage(convertToDouble(getValueFromXML(quote,
+				"ChangeFromTwoHundreddayMovingAverage")));
 		d.setAverageDailyVolume(convertToLong(getValueFromXML(quote, "AverageDailyVolume")));
 		d.setPERatio(convertToDouble(getValueFromXML(quote, "PERatio")));
 		d.setPEGRatio(convertToDouble(getValueFromXML(quote, "PEGRatio")));
@@ -140,7 +196,8 @@ public class YDataFetcher
 		db.setFiftydayMovingAverage(convertToDouble(getValueFromXML(quote, "FiftydayMovingAverage")));
 		db.setChangeFromFiftydayMovingAverage(convertToDouble(getValueFromXML(quote, "ChangeFromFiftydayMovingAverage")));
 		db.setTwoHundreddayMovingAverage(convertToDouble(getValueFromXML(quote, "TwoHundreddayMovingAverage")));
-		db.setChangeFromTwoHundreddayMovingAverage(convertToDouble(getValueFromXML(quote, "ChangeFromTwoHundreddayMovingAverage")));
+		db.setChangeFromTwoHundreddayMovingAverage(convertToDouble(getValueFromXML(quote,
+				"ChangeFromTwoHundreddayMovingAverage")));
 		db.setAverageDailyVolume(convertToLong(getValueFromXML(quote, "AverageDailyVolume")));
 		db.setPERatio(convertToDouble(getValueFromXML(quote, "PERatio")));
 		db.setPEGRatio(convertToDouble(getValueFromXML(quote, "PEGRatio")));
@@ -160,7 +217,8 @@ public class YDataFetcher
 
 		try
 		{
-			SerializePacked.writeToUnbuffered((new FileOutputStream(RunHelper.getTodayDataDirectory() + db.getSymbol() + ".data")).getChannel(), message);
+			SerializePacked.writeToUnbuffered((new FileOutputStream(RunHelper.getTodayDataDirectory() + db.getSymbol()
+					+ ".data")).getChannel(), message);
 		}
 		catch (IOException e)
 		{
@@ -176,7 +234,8 @@ public class YDataFetcher
 	public static String getValueFromXML(Element el, String tag)
 	{
 		NodeList nl = el.getElementsByTagName(tag);
-		if (nl != null && nl.getLength() > 0) return nl.item(0).getTextContent();
+		if (nl != null && nl.getLength() > 0)
+			return nl.item(0).getTextContent();
 		return null;
 	}
 
@@ -198,39 +257,41 @@ public class YDataFetcher
 	public static double convertToDouble(String value)
 	{
 		double d = 0.0;
-		if (value != null && !value.isEmpty()) try
-		{
-			double multiplier = 1.0;
-			if (value.contains("B"))
+		if (value != null && !value.isEmpty())
+			try
 			{
-				value = value.replace("B", "");
-				multiplier = 1000000000;
+				double multiplier = 1.0;
+				if (value.contains("B"))
+				{
+					value = value.replace("B", "");
+					multiplier = 1000000000;
+				}
+				else if (value.contains("M"))
+				{
+					value = value.replace("M", "");
+					multiplier = 1000000;
+				}
+				d = Double.valueOf(value) * multiplier;
 			}
-			else if (value.contains("M"))
+			catch (NumberFormatException e)
 			{
-				value = value.replace("M", "");
-				multiplier = 1000000;
+				e.printStackTrace();
 			}
-			d = Double.valueOf(value) * multiplier;
-		}
-		catch (NumberFormatException e)
-		{
-			e.printStackTrace();
-		}
 		return d;
 	}
 
 	public static long convertToLong(String value)
 	{
 		long l = 0;
-		if (value != null && !value.isEmpty()) try
-		{
-			l = Long.valueOf(value);
-		}
-		catch (NumberFormatException e)
-		{
-			e.printStackTrace();
-		}
+		if (value != null && !value.isEmpty())
+			try
+			{
+				l = Long.valueOf(value);
+			}
+			catch (NumberFormatException e)
+			{
+				e.printStackTrace();
+			}
 		return l;
 	}
 
