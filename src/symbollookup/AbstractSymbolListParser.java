7b9b5dc9f2cf5a722e636323778c2c67b4acbcc8
nchinmay <nchinmay@hotmail.com>
2015-02-22 02:26:20 -0500

    Initial commits on infracture to get and parse symbol lists from N_A_S_D_A_Q

---
+++ src/symbollookup/AbstractSymbolListParser.java
@@ -0,0 +1,10 @@
+package symbollookup;
+
+public abstract class AbstractSymbolListParser
+{
+	public static final String LINE_DELIM = "\\|";
+
+	public static final int MAX_SYMBOL_LEN = 5;
+	public static final String TEST_ISSUE = "Y";
+	public static final String NOT_TEST_ISSUE = "N";
+}
---
+++ src/symbollookup/NasdaqSymbolListParser.java
@@ -0,0 +1,52 @@
+package symbollookup;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class NasdaqSymbolListParser extends AbstractSymbolListParser
+{
+	public static final int SYMBOL_INDEX = 0;
+	public static final int SECURITY_NAME_INDEX = 1;
+	public static final int MARKET_CATEGORY_INDEX = 2;
+	public static final int TEST_ISSUE_INDEX = 3;
+	public static final int FINANCIAL_STATUS_INDEX = 4;
+	public static final int LOT_SIZE_INDEX = 5;
+
+	public void parseFile()
+	{
+
+	}
+
+	public static void parseLine(String line)
+	{
+		if (!StringUtils.isEmpty(line))
+		{
+			String[] parts = line.split(AbstractSymbolListParser.LINE_DELIM);
+			if (parts.length >= LOT_SIZE_INDEX)
+			{
+				String symbol = parts[SYMBOL_INDEX];
+				boolean isValidSymbol = symbol.length() > 0 && symbol.length() <= MAX_SYMBOL_LEN
+						&& StringUtils.isAlpha(symbol.subSequence(0, 1));
+				boolean isNotTestIssue = NOT_TEST_ISSUE.equals(parts[TEST_ISSUE_INDEX]);
+				if (isValidSymbol && isNotTestIssue)
+				{
+					// TODO - Do Stuff WIth Symbol
+					for (String s : parts)
+						System.out.println(s);
+				}
+			}
+		}
+	}
+
+	/**
+	 * TEST STUFF
+	 */
+	static String getTestLine()
+	{
+		return "AAL|American Airlines Group, Inc. - Common Stock|Q|N|N|100";
+	}
+
+	public static void main(String[] args)
+	{
+		NasdaqSymbolListParser.parseLine(getTestLine());
+	}
+}
