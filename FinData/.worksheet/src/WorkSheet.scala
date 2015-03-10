575955c8bf9d3f31e744d627b517050d133f3e41
Chinmay Nerurkar <nchinmay@hotmail.com>
2015-03-10 16:00:16 -0400

    gitignore

---
+++ FinData/.worksheet/src/WorkSheet.scala
@@ -0,0 +1,11 @@
+import findata.datafetching.YFHistDataFetcher
+import findata.toolset.FinTools
+
+object WorkSheet {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(173); 
+  val testSymbols = List("AAL", "DAL", "MU", "YHOO", "AA", "KORS", "TSLA");System.out.println("""testSymbols  : List[String] = """ + $show(testSymbols ));$skip(186); 
+
+  testSymbols.foreach {
+    s => val histStream = YFHistDataFetcher.getSortedHistDataStream(s, 20140309); println(FinTools.getSharpeRatio(FinTools.getIntervalReturns(histStream)))
+  }}
+
+}
