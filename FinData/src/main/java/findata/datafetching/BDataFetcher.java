dad009f3561263fd5f7e901535bb919c21cb8e6c
Chinmay Nerurkar <cnerurkar@appnexus.com>
2016-05-05 14:25:07 -0400

    re-structure FW.scala and clean up

--- FinData/src/main/java/findata/datafetching/BDataFetcher.java
+++
@@ -1,6 +0,0 @@
-package findata.datafetching;
-
-public class BDataFetcher
-{
-
-}
--- FinData/src/main/scala/findata/framework/FW.scala
+++ FinData/src/main/scala/findata/framework/FW.scala
@@ -8,23 +8,20 @@ import java.text.SimpleDateFormat
 import org.saddle.Frame
 import java.util.Date
 import org.apache.commons.math3.util.FastMath
+import findata.datafetching.YFFundamentalDataFetcherScala
+import org.saddle.Series
 
 object FW {
   val ADJ_CLOSE = "Adj Close"
-  val YHIST_DATA_SDF = new SimpleDateFormat("yyyy-MM-dd")
+  val YHIST_DATA_SDF = new SimpleDateFormat(YFHistDataFetcherScala.YH_DATA_DATE_FORMAT)
   val TRADING_DAYS_IN_YEAR: Integer = 252
   val TRADING_MONTHS_IN_YEAR: Integer = 12
-  val RISK_FREE_RETURN: Double = 0.06
+  val RISK_FREE_RATE: Double = 0.06
 
   def main(args: Array[String]): Unit = {
     val fw = new FW
-    val adjClose = fw.getYHistData("AAPL", 20140501).col(ADJ_CLOSE).mapVec { x => x.map { x => x.toDouble } }
-    println(adjClose)
-    val shiftedAdjClose = adjClose.shift(-1)
-    println(shiftedAdjClose)
-    val returns = (adjClose / shiftedAdjClose - 1)
-    println(returns)
-    val sharpe: Double = FastMath.sqrt(FW.TRADING_DAYS_IN_YEAR.toDouble) * (returns.colAt(0).mean - (RISK_FREE_RETURN / TRADING_DAYS_IN_YEAR)) / returns.colAt(0).stdev
+    val adjDailyCloses = (fw.getYHistData("AAPL", 20140501).col(ADJ_CLOSE).mapVec { x => x.map { x => x.toDouble } }).colAt(0)
+    val sharpe = fw.sharpeDailyRet(adjDailyCloses)
     println(sharpe)
   }
 
@@ -36,6 +33,18 @@ class FW {
     CsvParser.parse(csvSource).withRowIndex(0).withColIndex(0).mapRowIndex { x => FW.YHIST_DATA_SDF.parse(x) }
   }
 
-  //def returnize0(Frame[String, String, Double] f)  = {1}
+  def sharpeDailyRet(adjustedDailyCloses: Series[Date, Double]): Double = { sharpe(adjustedDailyCloses) }
+
+  def sharpeMonthlyRet(adjustedMonthlyCloses: Series[Date, Double]): Double = { sharpe(adjustedMonthlyCloses, numOfTradingPeriodsInYear = FW.TRADING_MONTHS_IN_YEAR) }
+
+  def sharpe(priceSeries: Series[Date, Double], riskFreeRate: Double = FW.RISK_FREE_RATE, numOfTradingPeriodsInYear: Integer = FW.TRADING_DAYS_IN_YEAR): Double = {
+    val dailyReturns = returnize(priceSeries)
+    val sharpe: Double = FastMath.sqrt(numOfTradingPeriodsInYear.toDouble) * (dailyReturns.mean - (riskFreeRate / numOfTradingPeriodsInYear)) / dailyReturns.stdev
+    sharpe
+  }
+
+  def returnize(data: Series[Date, Double], returnPeriod: Integer = -1): Series[Date, Double] = {
+    data / data.shift(returnPeriod) - 1
+  }
 
 }
