2139b3fb5142a6cfa7537d6cbdcff31f0d10c343
nchinmay <nchinmay@hotmail.com>
2016-05-07 18:30:12 -0400

    some progress

--- FinData/src/main/scala/findata/framework/FW.scala
+++ FinData/src/main/scala/findata/framework/FW.scala
@@ -10,41 +10,78 @@ import java.util.Date
 import org.apache.commons.math3.util.FastMath
 import findata.datafetching.YFFundamentalDataFetcherScala
 import org.saddle.Series
+import org.saddle.Index
+import org.saddle.index.OuterJoin
+import org.saddle.Vec
 
 object FW {
   val ADJ_CLOSE = "Adj Close"
   val YHIST_DATA_SDF = new SimpleDateFormat(YFHistDataFetcherScala.YH_DATA_DATE_FORMAT)
-  val TRADING_DAYS_IN_YEAR: Integer = 252
-  val TRADING_MONTHS_IN_YEAR: Integer = 12
+  val TRADING_DAYS_IN_YEAR: Int = 252
+  val TRADING_MONTHS_IN_YEAR: Int = 12
   val RISK_FREE_RATE: Double = 0.06
 
   def main(args: Array[String]): Unit = {
     val fw = new FW
-    val adjDailyCloses = (fw.getYHistData("AAPL", 20140501).col(ADJ_CLOSE).mapVec { x => x.map { x => x.toDouble } }).colAt(0)
-    val sharpe = fw.sharpeDailyRet(adjDailyCloses)
-    println(sharpe)
+
+    val symbols = Array("AAPL", "GOOG", "MSFT", "TSLA")
+    val from = 20140501
+
+    // TODO - Why doesn't joining 2 Frame[Date, String, Double]s return a Frame[Date, String, Double]? 
+    var ret: Frame[Date, String, Double] = fw.getAdjClosesNormalized(symbols, from)
+    println(ret)
+
+    val allocs = Frame(Vec(0.25, 0.25, 0.25, 0.25), ret.rowIx, ret.colIx)
+println( allocs)
+   // val weightedRet = ret.dot(allocs)
+    // val sharpe = fw.sharpeDailyRet(adjDailyCloses)
+    // println(sharpe)
   }
 
 }
 class FW {
-  def getYHistData(symbol: String, from: Integer): Frame[Date, String, String] = {
+  def getAdjClosesNormalized(symbols: Array[String], from: Int): Frame[Date, String, Double] = {
+    var ret: Frame[Date, Int, Double] = null
+    for (symbol <- symbols) {
+      if (ret == null) ret = getAdjClosesNormalized(symbol, from).resetColIndex
+      else ret = ret.join(getAdjClosesNormalized(symbol, from), OuterJoin)
+    }
+    ret.setColIndex(Index(symbols))
+  }
+
+  def getAdjClosesNormalized(symbol: String, from: Int): Frame[Date, String, Double] = {
+    val adjDailyCloses = (getYHistData(symbol, from).col(FW.ADJ_CLOSE).setColIndex(Index(symbol)).mapVec { x => x.map { x => x.toDouble } })
+    val adjClosesNormalized = adjDailyCloses.mapValues { x => x / adjDailyCloses.at(adjDailyCloses.rowIx.length - 1, 0) }
+    adjClosesNormalized
+  }
+
+  def getYHistData(symbol: String, from: Int): Frame[Date, String, String] = {
     val url = YFHistDataFetcherScala.getBaseUrl(symbol, from)
     val csvSource = new CsvUrl(url)
     CsvParser.parse(csvSource).withRowIndex(0).withColIndex(0).mapRowIndex { x => FW.YHIST_DATA_SDF.parse(x) }
   }
 
-  def sharpeDailyRet(adjustedDailyCloses: Series[Date, Double]): Double = { sharpe(adjustedDailyCloses) }
+  def sharpeDailyRet(adjustedDailyCloses: Series[Date, Double]): Double = {
+    val dailyReturns = returnizeSeries(adjustedDailyCloses)
+    sharpe(dailyReturns)
+  }
 
-  def sharpeMonthlyRet(adjustedMonthlyCloses: Series[Date, Double]): Double = { sharpe(adjustedMonthlyCloses, numOfTradingPeriodsInYear = FW.TRADING_MONTHS_IN_YEAR) }
+  def sharpeMonthlyRet(adjustedMonthlyCloses: Series[Date, Double]): Double = {
+    val monthlyReturns = returnizeSeries(adjustedMonthlyCloses)
+    sharpe(monthlyReturns, numOfTradingPeriodsInYear = FW.TRADING_MONTHS_IN_YEAR)
+  }
 
-  def sharpe(priceSeries: Series[Date, Double], riskFreeRate: Double = FW.RISK_FREE_RATE, numOfTradingPeriodsInYear: Integer = FW.TRADING_DAYS_IN_YEAR): Double = {
-    val dailyReturns = returnize(priceSeries)
-    val sharpe: Double = FastMath.sqrt(numOfTradingPeriodsInYear.toDouble) * (dailyReturns.mean - (riskFreeRate / numOfTradingPeriodsInYear)) / dailyReturns.stdev
+  def sharpe(returnSeries: Series[Date, Double], riskFreeRate: Double = FW.RISK_FREE_RATE, numOfTradingPeriodsInYear: Integer = FW.TRADING_DAYS_IN_YEAR): Double = {
+    val sharpe: Double = FastMath.sqrt(numOfTradingPeriodsInYear.toDouble) * (returnSeries.mean - (riskFreeRate / numOfTradingPeriodsInYear)) / returnSeries.stdev
     sharpe
   }
 
-  def returnize(data: Series[Date, Double], returnPeriod: Integer = -1): Series[Date, Double] = {
-    data / data.shift(returnPeriod) - 1
+  def returnizeSeries(dataSeries: Series[Date, Double], returnPeriod: Int = -1): Series[Date, Double] = {
+    dataSeries / dataSeries.shift(returnPeriod) - 1
+  }
+
+  def returnizeFrame(dataFrame: Frame[Date, String, Double], returnPeriod: Int = -1): Frame[Date, String, Double] = {
+    dataFrame / dataFrame.shift(returnPeriod) - 1
   }
 
 }
