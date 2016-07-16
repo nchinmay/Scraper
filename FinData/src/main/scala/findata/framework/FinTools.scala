3920a1eda406343d185aa0d6d93fed023bcd3045
Chinmay Nerurkar <cnerurkar@appnexus.com>
2016-07-16 16:52:22 -0400

    Market entry/exit timing test

--- FinData/src/main/scala/findata/framework/FinTools.scala
+++ FinData/src/main/scala/findata/framework/FinTools.scala
@@ -9,8 +9,13 @@ import org.saddle.index.OuterJoin
 import org.saddle.io._
 
 import findata.datafetching.YFHistDataFetcherScala
+import org.saddle.index.RightJoin
+import org.saddle.index.LeftJoin
 
-object FW {
+import breeze.optimize._
+import breeze.linalg.DenseVector
+
+object FinTools {
   val ADJ_CLOSE = "Adj Close"
   val YHIST_DATA_SDF = new SimpleDateFormat(YFHistDataFetcherScala.YH_DATA_DATE_FORMAT)
   val TRADING_DAYS_IN_YEAR: Int = 252
@@ -18,15 +23,59 @@ object FW {
   val RISK_FREE_RATE: Double = 0.06
 
   def main(args: Array[String]): Unit = {
-    val fw = new FW
+    val fw = new FinTools
 
-    val symbols = Array("AAPL", "GOOG", "MSFT", "TSLA")
-    val from = 20150510
+    val symbols = Array("AAPL", "MSFT", "GOOGL", "TSLA", "FB")
+
+    val from = 20130516
 
     // TODO - Why doesn't joining 2 Frame[Date, String, Double]s return a Frame[Date, String, Double]? 
-    val adjReturnsNorm: Frame[Date, String, Double] = fw.getAdjClosesNormalized(symbols, from)
-    println(adjReturnsNorm.head(5))
+    val adjCloseNorm: Frame[Date, String, Double] = fw.getAdjClosesNormalized(symbols, from)
+    // println(adjCloseNorm)
 
+    val dailyReturns = fw.returnizeFrame(adjCloseNorm)
+    println(dailyReturns)
+
+    val alloc = DenseVector(0.2, 0.2, 0.2, 0.2, 0.2)
+
+    val f = new DiffFunction[DenseVector[Double]] {
+      def calculate(x: DenseVector[Double]) = {
+        val a = Vec(x.toArray)
+        val sharpe = fw.sharpeDailyRet(dailyReturns, a)
+        (sharpe, x)
+      }
+    }
+
+    val lbfgs = new LBFGS[DenseVector[Double]](maxIter = 100, m = 3)
+
+    val optimum = lbfgs.minimize(f, alloc)
+
+    println(optimum)
+
+    //    val largeRet = dailyReturns.colAt(0).filter { x => Math.abs(x) >= 0.005 }
+    //    // println(largeRet)
+    //
+    //    val (l, r) = dailyReturns.shift(1).align(largeRet, RightJoin)
+    //
+    //    // val comp = dailyReturns.join(largeRet,OuterJoin)
+    //
+    //    println(l.length)
+    //
+    //    val add = l + r
+    //    val absAdd = l.mapValues { x => Math.abs(x) } + r.mapValues { x => Math.abs(x) }
+    //
+    //    val success = (l < r).filter { x => x }
+    //    val failure = (l < r).filter { x => !x }
+    //    println("Wins ->" + success.length)
+    //    println("Loses ->" + failure.length)
+    //
+    //    val absRet = (l - r).mapValues { x => Math.abs(x) }
+    //    val profit = absRet.mask((l < r).toVec).sum
+    //    val loss = absRet.mask((l >= r).toVec).sum
+    //
+    //    println("profit -> " + profit)
+    //    println("loss -> " + loss)
+    //    println("Net -> " + (profit - loss))
     //   val allocs = Vec(0.25, 0.25, 0.25, 0.25)
     //   val allocFrame = Frame(mat.repeat(allocs, adjReturnsNorm.rowIx.length, true)).setRowIndex(adjReturnsNorm.rowIx).setColIndex(adjReturnsNorm.colIx)
     //   val weightedAdjCloses = adjReturnsNorm.dot(allocFrame)
@@ -34,29 +83,29 @@ object FW {
     //   println("SHARPE = " + fw.sharpeDailyRet(adjReturnsNorm, allocs))
 
     // Brute force optimize 4 stock portfolio, no constraints
-    var bestSharpe: Double = 0
-    var bestAlloc = Vec.empty[Double]
-    for (i <- 1 to 100) {
-      for (j <- 0 to 100 - i) {
-        for (k <- 1 to 100 - i - j) {
-          for (l <- 0 to 100 - i - j - k) {
-            if (i + j + l + k == 100) {
-              val alloc = Vec(i / 100.0, j / 100.0, k / 100.0, l / 100.0)
-              val sharpe = fw.sharpeDailyRet(adjReturnsNorm, alloc)
-              if (sharpe > bestSharpe) {
-                bestSharpe = sharpe
-                bestAlloc = alloc
-              }
-            }
-          }
-        }
-      }
-    }
-    println("bestSharpe = " + bestSharpe)
-    println("bestAlloc = " + bestAlloc)
+    //    var bestSharpe: Double = 0
+    //    var bestAlloc = Vec.empty[Double]
+    //    for (i <- 1 to 100) {
+    //      for (j <- 0 to 100 - i) {
+    //        for (k <- 1 to 100 - i - j) {
+    //          for (l <- 0 to 100 - i - j - k) {
+    //            if (i + j + l + k == 100) {
+    //              val alloc = Vec(i / 100.0, j / 100.0, k / 100.0, l / 100.0)
+    //              val sharpe = fw.sharpeDailyRet(adjReturnsNorm, alloc)
+    //              if (sharpe > bestSharpe) {
+    //                bestSharpe = sharpe
+    //                bestAlloc = alloc
+    //              }
+    //            }
+    //          }
+    //        }
+    //      }
+    //    }
+    //    println("bestSharpe = " + bestSharpe)
+    //    println("bestAlloc = " + bestAlloc)
   }
 }
-class FW {
+class FinTools {
   /**
    * Adjusted Closes
    */
@@ -68,7 +117,7 @@ class FW {
   }
 
   def getAdjClosesNormalized(symbol: String, from: Int): Frame[Date, String, Double] = {
-    val adjDailyCloses = (getYHistData(symbol, from).col(FW.ADJ_CLOSE).setColIndex(Index(symbol)).mapVec { x => x.map { x => x.toDouble } })
+    val adjDailyCloses = (getYHistData(symbol, from).col(FinTools.ADJ_CLOSE).setColIndex(Index(symbol)).mapVec { x => x.map { x => x.toDouble } })
     val adjClosesNormalized = adjDailyCloses.mapValues { x => x / adjDailyCloses.at(adjDailyCloses.rowIx.length - 1, 0) }
     adjClosesNormalized
   }
@@ -79,7 +128,7 @@ class FW {
   def getYHistData(symbol: String, from: Int): Frame[Date, String, String] = {
     val url = YFHistDataFetcherScala.getBaseUrl(symbol, from)
     val csvSource = CsvUrl(url)
-    CsvParser.parse(csvSource).withRowIndex(0).withColIndex(0).mapRowIndex { x => FW.YHIST_DATA_SDF.parse(x) }
+    CsvParser.parse(csvSource).withRowIndex(0).withColIndex(0).mapRowIndex { x => FinTools.YHIST_DATA_SDF.parse(x) }
   }
 
   /**
@@ -104,10 +153,10 @@ class FW {
 
   def sharpeMonthlyRet(adjustedMonthlyCloses: Series[Date, Double]): Double = {
     val monthlyReturns = returnizeSeries(adjustedMonthlyCloses)
-    sharpe(monthlyReturns, numOfTradingPeriodsInYear = FW.TRADING_MONTHS_IN_YEAR)
+    sharpe(monthlyReturns, numOfTradingPeriodsInYear = FinTools.TRADING_MONTHS_IN_YEAR)
   }
 
-  def sharpe(returnSeries: Series[Date, Double], riskFreeRate: Double = FW.RISK_FREE_RATE, numOfTradingPeriodsInYear: Integer = FW.TRADING_DAYS_IN_YEAR): Double = {
+  def sharpe(returnSeries: Series[Date, Double], riskFreeRate: Double = FinTools.RISK_FREE_RATE, numOfTradingPeriodsInYear: Integer = FinTools.TRADING_DAYS_IN_YEAR): Double = {
     val excessReturns = returnSeries - (riskFreeRate / numOfTradingPeriodsInYear)
     val sharpe: Double = FastMath.sqrt(numOfTradingPeriodsInYear.toDouble) * excessReturns.mean / excessReturns.stdev
     sharpe
--- FinData/src/main/scala/findata/framework/WorkSheet.sc
+++ FinData/src/main/scala/findata/framework/WorkSheet.sc
@@ -1,39 +1,12 @@
-import findata.datafetching.YFHistDataFetcher
-import findata.datafetching.YFHistDataFetcherScala
-import findata.toolset.FinTools
-import findata.framework._
-import org.saddle.io._
+object WS {
+  val li = List(2734512, 2735900, 2865150, 2735621, 2735938, 2747418, 2794514, 2906136, 2858380, 2905590, 2747440, 2906137, 2747237, 2946572, 2958988, 2905589, 2794216, 2794511, 2851075, 2870788, 2870790, 2870783, 2870789, 2870784)
+                                                  //> li  : List[Int] = List(2734512, 2735900, 2865150, 2735621, 2735938, 2747418,
+                                                  //|  2794514, 2906136, 2858380, 2905590, 2747440, 2906137, 2747237, 2946572, 295
+                                                  //| 8988, 2905589, 2794216, 2794511, 2851075, 2870788, 2870790, 2870783, 2870789
+                                                  //| , 2870784)
+val s = li.sorted                                 //> s  : List[Int] = List(2734512, 2735621, 2735900, 2735938, 2747237, 2747418, 
+                                                  //| 2747440, 2794216, 2794511, 2794514, 2851075, 2858380, 2865150, 2870783, 2870
+                                                  //| 784, 2870788, 2870789, 2870790, 2905589, 2905590, 2906136, 2906137, 2946572,
+                                                  //|  2958988)
 
-object WorkSheet {
-  val testSymbols = List("AAL", "DAL", "MU", "YHOO", "AA", "KORS", "TSLA")
-                                                  //> testSymbols  : List[String] = List(AAL, DAL, MU, YHOO, AA, KORS, TSLA)
-
-  //  testSymbols.foreach {
-  //    s => val histStream = YFHistDataFetcher.getSortedHistDataStream(s, 20140309); println(FinTools.getSharpeRatio(FinTools.getIntervalReturns(histStream)))
-  //  }
-
-  // val csvStr = YFHistDataFetcherScala.getHistDataCSVStream("AAPL", 20150301).get.foreach(l => { l.foreach(s => print(s + "\t")); println })
-
-  val url = YFHistDataFetcherScala.getBaseUrl("AAPL", 20150301)
-                                                  //> url  : String = http://ichart.finance.yahoo.com/table.csv?s=AAPL&a=2&b=1&c=2
-                                                  //| 015
-
-  val csvSource = new CsvUrl(url)                 //> csvSource  : findata.framework.CsvUrl = CsvUrl(http://ichart.finance.yahoo.c
-                                                  //| om/table.csv?s=AAPL&a=2&b=1&c=2015, encoding: UTF-8)
-  val df = CsvParser.parse(csvSource).withRowIndex(0).withColIndex(0).col("Adj Close")
-                                                  //> df  : org.saddle.Frame[String,String,String] = [297 x 1]
-                                                  //|                Adj Close 
-                                                  //|               ---------- 
-                                                  //| 2016-05-03 ->      95.18 
-                                                  //| 2016-05-02 ->  93.639999 
-                                                  //| 2016-04-29 ->  93.739998 
-                                                  //| 2016-04-28 ->  94.830002 
-                                                  //| 2016-04-27 ->      97.82 
-                                                  //| ...
-                                                  //| 2015-03-06 -> 124.295889 
-                                                  //| 2015-03-05 -> 124.109352 
-                                                  //| 2015-03-04 -> 126.200576 
-                                                  //| 2015-03-03 -> 127.005659 
-                                                  //| 2015-03-02 -> 126.740569 
-                                                  //| 
 }
